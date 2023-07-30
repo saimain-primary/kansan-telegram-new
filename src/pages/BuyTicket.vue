51bbb29e74b61fb86da7e06d75b4b68efc51bdcc
Sai Main <saimain.primary@gmail.com>
2023-07-30 20:29:36 +0630

    init

--- src/pages/BuyTicket.vue
+++ src/pages/BuyTicket.vue
@@ -69,6 +69,44 @@ onMounted(async () => {
 <template>
   <div>
     <div class="bg-gray-900 mb-3 p-4 rounded text-gray-100">
+      <div class="mb-5 flex items-center">
+        <h3 class="font-medium font-padauk tracking-wide text-gray-100 text-sm">
+          Lucky နံပါတ်များ ရွေးချယ်ရန်
+        </h3>
+      </div>
+      <div>
+        <input
+          id="name"
+          placeholder="Lucky နံပါတ်ရှာမည်"
+          type="number"
+          max="999"
+          min="000"
+          class="w-full bg-gray-800 p-4 rounded appearance-none text-sm focus:outline-none focus:border-0"
+        />
+      </div>
+      <div class="text-center grid grid-cols-4 gap-2 pt-4">
+        <button
+          v-for="page in totalPages"
+          :key="page"
+          @click="changePage(page.value)"
+          class="text-xs py-1 font-padauk leading-5 border-blue-800 border hover:bg-blue-800 hover:text-white text-gray-100 rounded"
+          :class="{ 'bg-blue-800 text-white': currentPage === page.value }"
+        >
+          {{ page.text }}
+        </button>
+      </div>
+      <div class="grid grid-cols-10 gap-2 pt-3">
+        <button
+          v-for="number in currentPageNumbers"
+          :key="number"
+          :class="numberClass(number)"
+          @click.prevent="handlerSelectNumber(number)"
+        >
+          {{ number }}
+        </button>
+      </div>
+    </div>
+    <div class="bg-gray-900 mb-3 p-4 rounded text-gray-100">
       <div class="mb-3 gap-1 flex items-center">
         <h3 class="font-medium font-padauk tracking-wide text-gray-100 text-sm">
           ရွေးချယ်ထားသော Lucky နံပါတ်များ
@@ -78,7 +116,9 @@ onMounted(async () => {
         </div>
       </div>
       <div class="text-center pt-4 mb-3" v-if="selectedNumbers.length <= 0">
-        <p class="text-xs text-gray-400 font-padauk">သင့် Lucky နံပါတ်များ ရွေးချယ်ပါ</p>
+        <p class="text-xs text-gray-400 font-padauk">
+          သင့် Lucky နံပါတ်များ ရွေးချယ်ပါ
+        </p>
       </div>
       <div v-else class="mt-3 pt-4 mb-3">
         <div class="grid grid-cols-6 gap-2 mb-3">
@@ -104,44 +144,6 @@ onMounted(async () => {
             </svg>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="bg-gray-900 mb-3 p-4 rounded text-gray-100">
-      <div class="mb-3 flex items-center">
-        <h3 class="font-medium font-padauk tracking-wide text-gray-100 text-sm">
-          Lucky နံပါတ်များ ရွေးချယ်ရန်
-        </h3>
-      </div>
-      <div>
-        <input
-          id="name"
-          placeholder="Lucky နံပါတ်ရှာမည်"
-          type="number"
-          max="999"
-          min="000"
-          class="w-full bg-gray-800 p-4 rounded appearance-none text-sm focus:outline-none focus:border-0"
-        />
-      </div>
-      <div class="mt-4 text-center grid grid-cols-4 gap-2 pt-4 mb-5">
-        <button
-          v-for="page in totalPages"
-          :key="page"
-          @click="changePage(page.value)"
-          class="text-xs py-1 font-padauk leading-5 border-blue-800 border hover:bg-blue-800 hover:text-white text-gray-100 rounded"
-          :class="{ 'bg-blue-800 text-white': currentPage === page.value }"
-        >
-          {{ page.text }}
-        </button>
-      </div>
-      <div class="grid grid-cols-10 gap-2 pt-3">
-        <button
-          v-for="number in currentPageNumbers"
-          :key="number"
-          :class="numberClass(number)"
-          @click.prevent="handlerSelectNumber(number)"
-        >
-          {{ number }}
-        </button>
       </div>
     </div>
   </div>
